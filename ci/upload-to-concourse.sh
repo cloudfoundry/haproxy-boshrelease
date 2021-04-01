80dcca6ab438a3a2ccc7b349ed160a5639c3031d
Dominik Froehlich <Dominik.Frolic@gmail.com>
2021-04-01 16:13:50 +0200

    plain pipeline w/o spruce
    enable PR validations
    
    Use different PR status to distinguish from old CI

---
+++ ci/upload-to-concourse.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ -z "$CONCOURSE_TARGET" ]; then
+    echo "Missing CONCOURSE_TARGET env var."
+    exit 1
+fi
+
+if [ -z "$CONCOURSE_URL" ]; then
+    echo "Missing CONCOURSE_URL env var."
+    exit 1
+fi
+
+if [ -z "$CONCOURSE_USER" ]; then
+    echo "Missing CONCOURSE_USER env var."
+    exit 1
+fi
+
+if [ -z "$CONCOURSE_PASSWORD" ]; then
+    echo "Missing CONCOURSE_PASSWORD env var."
+    exit 1
+fi
+
+if [ ! -f "vars.yml" ]; then
+    echo "Missing vars.yml. Please create it first."
+    exit 1
+fi
+
+
+fly -t "$CONCOURSE_TARGET" login -c "$CONCOURSE_URL" -u "$CONCOURSE_USER" -p "$CONCOURSE_PASSWORD"
+fly -t "$CONCOURSE_TARGET" validate-pipeline -c pipeline-plain.yml
+fly -t "$CONCOURSE_TARGET" set-pipeline -p haproxy-boshrelease -c pipeline-plain.yml --load-vars-from vars.yml
+fly -t "$CONCOURSE_TARGET" expose-pipeline -p haproxy-boshrelease
+
+echo "Done."
