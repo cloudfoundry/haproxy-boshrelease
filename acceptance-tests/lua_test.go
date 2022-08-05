525069998d4e414e56c25fce163daaaa40e32fe5
Alexander Lais <alexander.lais@sap.com>
2022-08-05 13:56:08 +0200

    Add wait after uploading lua source file so haproxy can start

--- acceptance-tests/lua_test.go
+++ acceptance-tests/lua_test.go
@@ -4,24 +4,24 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+	"time"
 
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
 
 var _ = FDescribe("Lua scripting", func() {
 	It("Deploys haproxy with lua script", func() {
-		replyLuaTargetPath := "/var/vcap/packages/haproxy/lua_test.lua"
-		opsfileLua := fmt.Sprintf(`---
+		opsfileLua := `---
 # Enable Lua scripting
 - type: replace
   path: /instance_groups/name=haproxy/jobs/name=haproxy/properties/ha_proxy/lua_scripts?/-
-  value: "%s"
+  value: "/var/vcap/packages/haproxy/lua_test.lua"
 - type: replace
   path: /instance_groups/name=haproxy/jobs/name=haproxy/properties/ha_proxy/frontend_config?
   value: |-
     acl lua_path path /lua_test
-    http-request use-service lua.lua_test if lua_path
-`, replyLuaTargetPath)
+    http-request use-service lua.lua_test if lua_path`
 
 		replyLuaContent := `
 local function lua_test(applet)
@@ -44,20 +44,33 @@ core.register_service("lua_test", "http", lua_test)
 		`
 
 		haproxyBackendPort := 12000
+		// Expect initial deployment to be failing due to lack of healthy backends
 		haproxyInfo, _ := deployHAProxy(baseManifestVars{
 			haproxyBackendPort:    haproxyBackendPort,
 			haproxyBackendServers: []string{"127.0.0.1"},
 			deploymentName:        deploymentNameForTestNode(),
-		}, []string{opsfileLua}, map[string]interface{}{}, true)
+		}, []string{opsfileLua}, map[string]interface{}{}, false)
+
+		// Verify that is in a failing state
+		Expect(boshInstances(deploymentNameForTestNode())[0].ProcessState).To(Or(Equal("failing"), Equal("unresponsive agent")))
 
 		// upload Lua script file
-		uploadFile(haproxyInfo, strings.NewReader(replyLuaContent), replyLuaTargetPath)
+		uploadFile(haproxyInfo, strings.NewReader(replyLuaContent), "/var/vcap/packages/haproxy/lua_test.lua")
 
 		closeLocalServer, localPort := startDefaultTestServer()
 		defer closeLocalServer()
 
 		closeTunnel := setupTunnelFromHaproxyToTestServer(haproxyInfo, haproxyBackendPort, localPort)
 		defer closeTunnel()
+
+		By("Waiting monit to report HAProxy is now healthy (due to having a healthy backend instance)")
+		// Since the backend is now listening, HAProxy healthcheck should start returning healthy
+		// and monit should in turn start reporting a healthy process
+		// We will up to wait one minute for the status to stabilise
+		Eventually(func() string {
+			return boshInstances(deploymentNameForTestNode())[0].ProcessState
+		}, time.Minute, time.Second).Should(Equal("running"))
+
 
 		By("Sending a request to HAProxy with Lua endpoint")
 		resp, err := http.Get(fmt.Sprintf("http://%s/lua_test", haproxyInfo.PublicIP))
