2fcbba27e92a4ab7225ee5c1a1e5e9c7f34663ad
Alexander Lais <alexander.lais@sap.com>
2022-09-08 08:03:41 +0200

    Add cgroupsv1 check for Docker for Mac
    Cancel local acceptance test attempt when there are changes in the repo
    The script would eventually error out after a few minutes for the same reason.
    
    Provide a script in the README.md that can be run by the operator to restart Docker for Mac with cgroupsv1 at their convenience.

--- acceptance-tests/run-local.sh
+++ acceptance-tests/run-local.sh
@@ -5,6 +5,32 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "$0")/" && pwd)"
 REPO_DIR="$(cd "$SCRIPT_DIR/.." && pwd)"
 
+if [ "$(git status -s | wc -l)" -gt 0 ]; then
+    echo "You have changes in your Git repository. Commit or clean (e.g. git clean -f) before running."
+    echo "The build will fail otherwise."
+    echo "Git Status:"
+    git status
+    exit 1
+fi
+
+docker_mac_check_cgroupsv1() {
+    # Force cgroups v1 on Docker for Mac
+    # inspired by https://github.com/docker/for-mac/issues/6073#issuecomment-1018793677
+
+    SETTINGS=~/Library/Group\ Containers/group.com.docker/settings.json
+
+    cgroupsV1Enabled=$(jq '.deprecatedCgroupv1' "$SETTINGS")
+    if [ "$cgroupsV1Enabled" != "true" ]; then 
+        echo "deprecatedCgroupv1 should be enabled in $SETTINGS. Otherwise the acceptance tests will not run on Docker for Mac."
+        echo "Check in the README.md for a convenient script to set deprecatedCgroupv1 and restart Docker."
+        exit 1
+    fi
+}
+
+if [ "$(uname)" == "Darwin" ]; then
+    docker_mac_check_cgroupsv1
+fi
+
 # Build acceptance test image
 pushd "$SCRIPT_DIR/../ci" || exit 1
  docker build -t haproxy-boshrelease-testflight .
