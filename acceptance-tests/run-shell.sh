34f97abec04976587fe69c44cafa5a0fb40726ce
Lotfi Kecir <lotfi.kecir@sap.com>
2022-10-20 16:02:56 +0200

    Use cpi docker bosh image

--- acceptance-tests/run-shell.sh
+++ acceptance-tests/run-shell.sh
@@ -6,9 +6,9 @@ SCRIPT_DIR="$(cd "$(dirname "$0")/" && pwd)"
 REPO_DIR="$(cd "$SCRIPT_DIR/.." && pwd)"
 
 # Build acceptance test image
-pushd "$SCRIPT_DIR/../ci" || exit 1
- docker build -t haproxy-boshrelease-testflight .
-popd || exit 1
+#pushd "$SCRIPT_DIR/../ci" || exit 1
+# docker build -t haproxy-boshrelease-testflight .
+#popd || exit 1
 
 # Run local shell
-docker run -it --rm --privileged -v "$REPO_DIR":/repo -e REPO_ROOT=/repo haproxy-boshrelease-testflight bash -c "cd /repo/ci/scripts && ./shell"
+docker run -it --rm --privileged -v "$REPO_DIR":/repo -e REPO_ROOT=/repo bosh/docker-cpi:main bash -c "cd /repo/ci/scripts && ./shell"
