9c61ce59391ac793ff978b359855d86d10069dac
Alexander Lais <alexander.lais@sap.com>
2022-10-26 17:55:18 +0200

    Prepare acceptance tests to use canned bosh director

--- ci/new/build-canned-docker.sh
+++ ci/new/build-canned-docker.sh
@@ -2,7 +2,10 @@
 
 BUILD_CONTAINER=bosh-director-build-$(date +%s)-$RANDOM
 
-docker run -d -e BOSH_CERT_DIR=/tmp/certs --privileged --name $BUILD_CONTAINER bosh/docker-cpi:main sleep infinity
+BOSH_DIRECTOR_TAG=${1:-bosh-director-docker}
+BASE_IMAGE=${2:-bosh/docker-cpi:main}
+
+docker run -d -e BOSH_CERT_DIR=/tmp/certs --privileged --name "$BUILD_CONTAINER" "$BASE_IMAGE" sleep infinity
 
 docker exec -e BOSH_CERT_DIR=/tmp/certs $BUILD_CONTAINER bash -c '
     sed -i '"'"'s#certs_dir=$(mktemp -d)#certs_dir=${BOSH_CERT_DIR:-$(mktemp -d /tmp/certs.XXXX)}#'"'"' $(command -v start-bosh)
@@ -36,34 +39,6 @@ docker exec -e BOSH_CERT_DIR=/tmp/certs $BUILD_CONTAINER bash -c '
 
 docker stop "$BUILD_CONTAINER"
 
-docker tag $(docker commit "$BUILD_CONTAINER") bosh-director-docker 
+docker tag $(docker commit "$BUILD_CONTAINER") "$BOSH_DIRECTOR_TAG"
 
 docker rm -f "$BUILD_CONTAINER"
-
-docker run -d --privileged --name canned-bosh bosh-director-docker
-
-docker exec -t -e BOSH_CERT_DIR=/tmp/certs canned-bosh bash -l -c '
-    source start-bosh
-    generate_certs $BOSH_CERT_DIR
-
-    echo '"'"'export OUTER_CONTAINER_IP=$(ip route get 8.8.8.8 | head -n1 | cut -d " " -f 8)'"'"' >> /etc/profile
-    echo '"'"'export DOCKER_HOST=tcp://$OUTER_CONTAINER_IP:4243'"'"' >> /etc/profile
-    echo "export DOCKER_TLS_VERIFY=1" >> /etc/profile
-    echo "export DOCKER_CERT_PATH=$BOSH_CERT_DIR" >> /etc/profile
-
-    source /etc/profile
-
-    service docker start || echo "Failed to start Docker!"
-
-    while ! docker ps > /dev/null; do echo "Waiting for Docker to start"; sleep 1; done;
-
-    export BOSH0_CONTAINER=$(jq .current_vm_cid -r /tmp/local-bosh/director/state.json)
-
-    docker start "$BOSH0_CONTAINER"
-
-    while [ "$(docker container ls -a --format "{{json .State}}" --filter name=$BOSH0_CONTAINER | jq . -r)" != "running" ]; do echo Waiting for bosh/0 container to start; sleep 1; done;
-
-    docker exec "$BOSH0_CONTAINER" /var/vcap/bosh/bin/monit start all
-    docker exec "$BOSH0_CONTAINER" bash -c "while ! ( /var/vcap/bosh/bin/monit summary | grep -E '"'"'System.*running$'"'"' ); do echo Waiting for monit to start all jobs; sleep 1; done"
-    bosh env
-'
