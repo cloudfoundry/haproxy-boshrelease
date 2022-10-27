15719634dec8d0d43bd5e28c5a9407433ba1944f
Alexander Lais <alexander.lais@sap.com>
2022-10-27 17:32:57 +0200

    Add retries and fallbacks

--- ci/scripts/start-bosh.sh
+++ ci/scripts/start-bosh.sh
@@ -13,25 +13,33 @@ source /tmp/local-bosh/director/env
 
 export
 
-service docker start || ( echo "Failed to start Docker!"
-  echo --- docker log ---
-  tail -n 50 /var/log/docker.log
-  echo --- / docker log ---
-  exit 1
-)
+if ! docker ps &>/dev/null; then
+  service docker start || ( echo "Failed to start Docker!"
+    echo --- docker log ---
+    tail -n 50 /var/log/docker.log
+    echo --- / docker log ---
+    exit 1
+  )
+fi
 
+docker_retries=10
 while ! docker ps &>/dev/null; do 
   echo "Waiting for Docker to start"
   sleep 1
+  docker_retries=$(($docker_retries - 1))
+  if [ $docker_retries -eq 0 ]; then
+    echo "Starting Docker failed"
+    echo --- docker log ---
+    tail -n 20 /var/log/docker.log
+    echo --- / docker log ---
+    exit 1
+  fi
 done
 
-echo --- docker log ---
-tail -n 50 /var/log/docker.log
-echo --- / docker log ---
 
 export BOSH0_CONTAINER=$(jq .current_vm_cid -r /tmp/local-bosh/director/state.json)
 
-docker start "$BOSH0_CONTAINER"
+docker start "$BOSH0_CONTAINER" || (echo could not start bosh/0 && exit 1)
 
 while [ "$(docker container ls -a --format "{{json .State}}" --filter name=$BOSH0_CONTAINER | jq . -r)" != "running" ]; do 
   echo "Waiting for bosh/0 container to start"
