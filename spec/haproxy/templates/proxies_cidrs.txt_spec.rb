0620360bdb2a2411a76acd3b6503c059efed24c4
M Rizwan Shaik <m.rizwan.shaik@sap.com>
2024-09-10 12:42:11 +0200

    unit test for proxies_cidr.txt.erb

--- spec/haproxy/templates/proxies_cidrs.txt_spec.rb
+++ spec/haproxy/templates/proxies_cidrs.txt_spec.rb
@@ -5,44 +5,29 @@ require 'rspec'
 describe 'config/proxies_cidrs.txt' do
   let(:template) { haproxy_job.template('config/proxies_cidrs.txt') }
 
-  describe 'ha_proxy.proxies_cidrs' do
-    context 'when a space-separated list of cidrs is provided' do
+
+  context 'when ha_proxy.expect_proxy is provided ' do
+    context 'when an array of cidrs is provided' do
       it 'has the correct contents' do
         expect(template.render({
           'ha_proxy' => {
-            'proxies_cidrs' => '10.0.1.32/27
-                                2001:db8::/32
+            'expect_proxy' => [
+              '10.0.1.32/27',
+              '2600:1f18:4c0b:3207::/64'
+            ]
           }
         })).to eq(<<~EXPECTED)
           # generated from proxies_cidrs.txt.erb
 
           # BEGIN proxies cidrs
+          # detected cidrs provided as array in cleartext format
             10.0.1.32/27
-            2001:db8::/32
+            2600:1f18:4c0b:3207::/64
           # END proxies cidrs
 
         EXPECTED
       end
     end
-
-    context 'when a newline-separated, gzipped, base64-encoded list of cidrs is provided' do
-      it 'has the correct contents' do
-        expect(template.render({
-          'ha_proxy' => {
-            'proxies_cidrs' => gzip_and_b64_encode("10.0.1.32/27\n2001:db8::/32")
-          }
-        })).to eq(<<~EXPECTED)
-          # generated from proxies_cidrs.txt.erb
-
-          # BEGIN proxies cidrs
-          10.0.1.32/27
-          2001:db8::/32
-          # END proxies cidrs
-
-        EXPECTED
-      end
-    end
-
     context 'when ha_proxy.proxies_cidrs is not provided' do
       it 'is empty' do
         expect(template.render({})).to be_a_blank_string
