8d28a003163184572ed2b53d1961deccd7d2daa0
Michal Tekel <mtekel@gmail.com>
2024-11-22 10:47:35 +0000

    ci: pipeline upload - login only when necessary

--- ci/upload-to-concourse.sh
+++ ci/upload-to-concourse.sh
@@ -9,7 +9,8 @@ fi
 
 echo "Please follow https://github.com/cloudfoundry/routing-concourse/blob/main/README.md#authentication-to-concourse for login."
 
-fly -t networking-extensions login -c https://concourse.arp.cloudfoundry.org/
+! fly status -t networking-extensions >/dev/null && \
+    fly -t networking-extensions login -c https://concourse.arp.cloudfoundry.org/
 fly -t networking-extensions validate-pipeline -c pipeline.yml
 fly -t networking-extensions set-pipeline -p haproxy-boshrelease -c pipeline.yml --load-vars-from vars.yml
 fly -t networking-extensions expose-pipeline -p haproxy-boshrelease
