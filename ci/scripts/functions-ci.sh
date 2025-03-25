8b88f32dcdb74f3b9690bfe052cd436e917bb3f8
Michal Tekel <mtekel@gmail.com>
2025-03-25 15:05:47 +0000

    test: dry bosh operations

--- ci/scripts/functions-ci.sh
+++ ci/scripts/functions-ci.sh
@@ -50,4 +50,31 @@ function git_pull() {
     git submodule update --init --recursive --force
 }
 
-echo "Loaded shell script functions..."
+function bosh_release() {
+    echo "----- Creating candidate BOSH release..."
+    bosh -n reset-release # in case dev_releases/ is in repo accidentally
+
+    bosh create-release
+    bosh upload-release --rebase
+    release_final_version=$(spruce json dev_releases/*/index.yml | jq -r ".builds[].version" | sed -e "s%+.*%%")
+    export RELEASE_VERSION="${release_final_version}.latest"
+    echo "----- Created ${RELEASE_VERSION}"
+}
+
+function bosh_assets() {
+    stemcell_jammy_path=$START_DIR/stemcell/*.tgz
+    stemcell_bionic_path=$START_DIR/stemcell-bionic/*.tgz
+
+    echo "----- Uploading Jammy stemcell"
+    bosh -n upload-stemcell $stemcell_jammy_path
+
+    echo "----- Uploading Bionic stemcell"
+    bosh -n upload-stemcell $stemcell_bionic_path
+
+    echo "----- Uploading os-conf (used for tests only)"
+    bosh -n upload-release --sha1 386293038ae3d00813eaa475b4acf63f8da226ef \
+    https://bosh.io/d/github.com/cloudfoundry/os-conf-release?v=22.1.2
+
+    export BOSH_PATH=$(which bosh)
+    export BASE_MANIFEST_PATH="$PWD/manifests/haproxy.yml"
+}
