b63e80e373d111c07839b8a764c973f4dd534402
Alexander Nicke <alexander.nicke@sap.com>
2025-04-08 16:38:43 +0200

    Improve true_client_ip acceptance test

--- acceptance-tests/true_client_ip_test.go
+++ acceptance-tests/true_client_ip_test.go
@@ -10,14 +10,18 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("True Client IP - forward_only_if_route_service", func() {
-	opsfileHeaders := `---
+const TrueClientIpHeader string = "X-CF-True-Client-IP"
+const MockClientIp string = "8.8.8.8"
+
+var _ = Describe("True Client IP", func() {
+
+	opsFileTrueClientIp := fmt.Sprintf(`---
 - type: replace
   path: /instance_groups/name=haproxy/jobs/name=haproxy/properties/ha_proxy/true_client_ip_header?
-  value: "X-CF-True-Client-IP"
+  value: "%s"
 - type: replace
   path: /instance_groups/name=haproxy/jobs/name=haproxy/properties/ha_proxy/forward_true_client_ip_header?
-  value: "forward_only_if_route_service"
+  value: ((forward_true_client_ip_header))
 # Configure CA and cert chain
 - type: replace
   path: /instance_groups/name=haproxy/jobs/name=haproxy/properties/ha_proxy/crt_list?/-
@@ -45,7 +49,8 @@ var _ = Describe("True Client IP - forward_only_if_route_service", func() {
       ca: https_frontend_ca
       common_name: haproxy.internal
       alternative_names: [haproxy.internal]
-`
+`, TrueClientIpHeader)
+
 	var closeLocalServer func()
 	var closeTunnel func()
 	var creds struct {
@@ -57,17 +62,17 @@ var _ = Describe("True Client IP - forward_only_if_route_service", func() {
 	}
 	var client *http.Client
 	var recordedHeaders http.Header
-	var request *http.Request
 	var err error
+	var deployVars map[string]interface{}
 
-	BeforeEach(func() {
+	JustBeforeEach(func() {
 		haproxyBackendPort := 12000
-		var varsStoreReader varsStoreReader
+
 		haproxyInfo, varsStoreReader := deployHAProxy(baseManifestVars{
 			haproxyBackendPort:    haproxyBackendPort,
 			haproxyBackendServers: []string{"127.0.0.1"},
 			deploymentName:        deploymentNameForTestNode(),
-		}, []string{opsfileHeaders}, map[string]interface{}{}, true)
+		}, []string{opsFileTrueClientIp}, deployVars, true)
 
 		err = varsStoreReader(&creds)
 		Expect(err).NotTo(HaveOccurred())
@@ -94,311 +99,102 @@ var _ = Describe("True Client IP - forward_only_if_route_service", func() {
 		closeTunnel()
 	})
 
-	It("Adds a header with the provided name and correct value for the true client ip, if it doesn't exist", func() {
-		ipAddresses := getAllIpAddresses()
-		request, err = http.NewRequest("GET", "https://haproxy.internal:443", nil)
-		Expect(err).NotTo(HaveOccurred())
-
-		By("Correctly sets the True-Client-Ip Header")
-		resp, err := client.Do(request)
-		expect200(resp, err)
-		headerKey := "X-Cf-True-Client-Ip"
-		Expect(recordedHeaders).To(HaveKey(headerKey))
-		Expect(recordedHeaders[headerKey]).To(HaveLen(1))
-		Expect(ipAddresses).To(ContainElement(recordedHeaders[headerKey][0]))
-	})
-
-	It("Overwrites the True-Client-Ip header if it is already set AND the request is not a route-service", func() {
-		ipAddresses := getAllIpAddresses()
-		request, err = http.NewRequest("GET", "https://haproxy.internal:443", nil)
-		request.Header.Set("X-Cf-True-Client-Ip", "8.8.8.8")
-		Expect(err).NotTo(HaveOccurred())
-
-		By("Overwrites the existing True-Client-Ip Header")
-		resp, err := client.Do(request)
-		expect200(resp, err)
-		headerKey := "X-Cf-True-Client-Ip"
-		Expect(recordedHeaders).To(HaveKey(headerKey))
-		Expect(recordedHeaders[headerKey]).To(HaveLen(1))
-		Expect(ipAddresses).To(ContainElement(recordedHeaders[headerKey][0]))
-	})
-
-	It("Does not overwrite the True-Client-Ip header if it is already set AND the request is a route-service", func() {
-		request, err = http.NewRequest("GET", "https://haproxy.internal:443", nil)
-		// Mock a route-service request via the X-Cf-Proxy-Signature header
-		request.Header.Set("X-Cf-Proxy-Signature", "abc123")
-		request.Header.Set("X-Cf-True-Client-Ip", "8.8.8.8")
-		Expect(err).NotTo(HaveOccurred())
-
-		By("Correctly preserves the pre-existing True-Client-Ip Header")
-		resp, err := client.Do(request)
-		expect200(resp, err)
-		headerKey := "X-Cf-True-Client-Ip"
-		Expect(recordedHeaders).To(HaveKey(headerKey))
-		Expect(recordedHeaders[headerKey]).To(HaveLen(1))
-		Expect(recordedHeaders[headerKey][0]).To(Equal("8.8.8.8"))
-	})
-})
-
-var _ = Describe("True Client IP - always_forward", func() {
-	opsfileHeaders := `---
-- type: replace
-  path: /instance_groups/name=haproxy/jobs/name=haproxy/properties/ha_proxy/true_client_ip_header?
-  value: "X-CF-True-Client-IP"
-- type: replace
-  path: /instance_groups/name=haproxy/jobs/name=haproxy/properties/ha_proxy/forward_true_client_ip_header?
-  value: "always_forward"
-# Configure CA and cert chain
-- type: replace
-  path: /instance_groups/name=haproxy/jobs/name=haproxy/properties/ha_proxy/crt_list?/-
-  value:
-    snifilter:
-    - haproxy.internal
-    ssl_pem:
-      cert_chain: ((https_frontend.certificate))((https_frontend_ca.certificate))
-      private_key: ((https_frontend.private_key))
-# Declare certs
-- type: replace
-  path: /variables?/-
-  value:
-    name: https_frontend_ca
-    type: certificate
-    options:
-      is_ca: true
-      common_name: bosh
-- type: replace
-  path: /variables?/-
-  value:
-    name: https_frontend
-    type: certificate
-    options:
-      ca: https_frontend_ca
-      common_name: haproxy.internal
-      alternative_names: [haproxy.internal]
-`
-	var closeLocalServer func()
-	var closeTunnel func()
-	var creds struct {
-		HTTPSFrontend struct {
-			Certificate string `yaml:"certificate"`
-			PrivateKey  string `yaml:"private_key"`
-			CA          string `yaml:"ca"`
-		} `yaml:"https_frontend"`
-	}
-	var client *http.Client
-	var recordedHeaders http.Header
-	var request *http.Request
-	var err error
-
-	BeforeEach(func() {
-		haproxyBackendPort := 12000
-		var varsStoreReader varsStoreReader
-		haproxyInfo, varsStoreReader := deployHAProxy(baseManifestVars{
-			haproxyBackendPort:    haproxyBackendPort,
-			haproxyBackendServers: []string{"127.0.0.1"},
-			deploymentName:        deploymentNameForTestNode(),
-		}, []string{opsfileHeaders}, map[string]interface{}{}, true)
-
-		err = varsStoreReader(&creds)
-		Expect(err).NotTo(HaveOccurred())
-
-		By("Starting a local http server to act as a backend")
-		var localPort int
-		closeLocalServer, localPort, err = startLocalHTTPServer(nil, func(w http.ResponseWriter, r *http.Request) {
-			writeLog("Backend server handling incoming request")
-			recordedHeaders = r.Header
-			_, _ = w.Write([]byte("OK"))
+	Context("forward_true_client_ip_header: forward_only_if_route_service", func() {
+		BeforeEach(func() {
+			deployVars = map[string]interface{}{
+				"forward_true_client_ip_header": "forward_only_if_route_service",
+			}
 		})
-		Expect(err).NotTo(HaveOccurred())
 
-		closeTunnel = setupTunnelFromHaproxyToTestServer(haproxyInfo, haproxyBackendPort, localPort)
-		client = buildHTTPClient(
-			[]string{creds.HTTPSFrontend.CA},
-			map[string]string{"haproxy.internal:443": fmt.Sprintf("%s:443", haproxyInfo.PublicIP)},
-			[]tls.Certificate{}, "",
-		)
-	})
+		It("Correctly handles True Client IP when forward_only_if_route_service option is selected", func() {
+			ipAddresses := getAllIpAddresses()
 
-	AfterEach(func() {
-		closeLocalServer()
-		closeTunnel()
-	})
+			By("Adding a header with the provided name and correct value for the True Client ip, if it doesn't exist in a request")
+			performRequest(client, false, false)
+			expectTrueClientHeader(recordedHeaders, ipAddresses) // Header is added
 
-	It("Adds a header with the provided name and correct value for the true client ip, if it doesn't exist", func() {
-		ipAddresses := getAllIpAddresses()
-		request, err = http.NewRequest("GET", "https://haproxy.internal:443", nil)
-		Expect(err).NotTo(HaveOccurred())
+			By("Overwriting the True-Client-Ip header if it is already set AND the request is NOT a route-service")
+			performRequest(client, false, true)
+			expectTrueClientHeader(recordedHeaders, ipAddresses) // Header is overwritten
 
-		By("Correctly sets the True-Client-Ip Header")
-		resp, err := client.Do(request)
-		expect200(resp, err)
-		headerKey := "X-Cf-True-Client-Ip"
-		Expect(recordedHeaders).To(HaveKey(headerKey))
-		Expect(recordedHeaders[headerKey]).To(HaveLen(1))
-		Expect(ipAddresses).To(ContainElement(recordedHeaders[headerKey][0]))
-	})
-
-	It("Does not overwrite the True-Client-Ip header if it is already set AND the request is not a route-service", func() {
-		request, err = http.NewRequest("GET", "https://haproxy.internal:443", nil)
-		request.Header.Set("X-Cf-True-Client-Ip", "8.8.8.8")
-		Expect(err).NotTo(HaveOccurred())
-
-		By("Correctly preserves the pre-existing True-Client-Ip Header")
-		resp, err := client.Do(request)
-		expect200(resp, err)
-		headerKey := "X-Cf-True-Client-Ip"
-		Expect(recordedHeaders).To(HaveKey(headerKey))
-		Expect(recordedHeaders[headerKey]).To(HaveLen(1))
-		Expect(recordedHeaders[headerKey][0]).To(Equal("8.8.8.8"))
-	})
-
-	It("Does not overwrite the True-Client-Ip header if it is already set AND the request is a route-service", func() {
-		request, err = http.NewRequest("GET", "https://haproxy.internal:443", nil)
-		// Mock a route-service request via the X-Cf-Proxy-Signature header
-		request.Header.Set("X-Cf-Proxy-Signature", "abc123")
-		request.Header.Set("X-Cf-True-Client-Ip", "8.8.8.8")
-		Expect(err).NotTo(HaveOccurred())
-
-		By("Correctly preserves the pre-existing True-Client-Ip Header")
-		resp, err := client.Do(request)
-		expect200(resp, err)
-		headerKey := "X-Cf-True-Client-Ip"
-		Expect(recordedHeaders).To(HaveKey(headerKey))
-		Expect(recordedHeaders[headerKey]).To(HaveLen(1))
-		Expect(recordedHeaders[headerKey][0]).To(Equal("8.8.8.8"))
-	})
-})
-
-var _ = Describe("True Client IP - always_set", func() {
-	opsfileHeaders := `---
-- type: replace
-  path: /instance_groups/name=haproxy/jobs/name=haproxy/properties/ha_proxy/true_client_ip_header?
-  value: "X-CF-True-Client-IP"
-- type: replace
-  path: /instance_groups/name=haproxy/jobs/name=haproxy/properties/ha_proxy/forward_true_client_ip_header?
-  value: "always_set"
-# Configure CA and cert chain
-- type: replace
-  path: /instance_groups/name=haproxy/jobs/name=haproxy/properties/ha_proxy/crt_list?/-
-  value:
-    snifilter:
-    - haproxy.internal
-    ssl_pem:
-      cert_chain: ((https_frontend.certificate))((https_frontend_ca.certificate))
-      private_key: ((https_frontend.private_key))
-# Declare certs
-- type: replace
-  path: /variables?/-
-  value:
-    name: https_frontend_ca
-    type: certificate
-    options:
-      is_ca: true
-      common_name: bosh
-- type: replace
-  path: /variables?/-
-  value:
-    name: https_frontend
-    type: certificate
-    options:
-      ca: https_frontend_ca
-      common_name: haproxy.internal
-      alternative_names: [haproxy.internal]
-`
-	var closeLocalServer func()
-	var closeTunnel func()
-	var creds struct {
-		HTTPSFrontend struct {
-			Certificate string `yaml:"certificate"`
-			PrivateKey  string `yaml:"private_key"`
-			CA          string `yaml:"ca"`
-		} `yaml:"https_frontend"`
-	}
-	var client *http.Client
-	var recordedHeaders http.Header
-	var request *http.Request
-	var err error
-
-	BeforeEach(func() {
-		haproxyBackendPort := 12000
-		var varsStoreReader varsStoreReader
-		haproxyInfo, varsStoreReader := deployHAProxy(baseManifestVars{
-			haproxyBackendPort:    haproxyBackendPort,
-			haproxyBackendServers: []string{"127.0.0.1"},
-			deploymentName:        deploymentNameForTestNode(),
-		}, []string{opsfileHeaders}, map[string]interface{}{}, true)
-
-		err = varsStoreReader(&creds)
-		Expect(err).NotTo(HaveOccurred())
-
-		By("Starting a local http server to act as a backend")
-		var localPort int
-		closeLocalServer, localPort, err = startLocalHTTPServer(nil, func(w http.ResponseWriter, r *http.Request) {
-			writeLog("Backend server handling incoming request")
-			recordedHeaders = r.Header
-			_, _ = w.Write([]byte("OK"))
+			By("NOT overwriting the True-Client-Ip header if it is already set AND the request is a route-service")
+			performRequest(client, true, true)
+			expectTrueClientHeader(recordedHeaders, []string{MockClientIp}) // Header is forwarded
 		})
-		Expect(err).NotTo(HaveOccurred())
-
-		closeTunnel = setupTunnelFromHaproxyToTestServer(haproxyInfo, haproxyBackendPort, localPort)
-		client = buildHTTPClient(
-			[]string{creds.HTTPSFrontend.CA},
-			map[string]string{"haproxy.internal:443": fmt.Sprintf("%s:443", haproxyInfo.PublicIP)},
-			[]tls.Certificate{}, "",
-		)
 	})
 
-	AfterEach(func() {
-		closeLocalServer()
-		closeTunnel()
+	Context("forward_true_client_ip_header: always_forward", func() {
+		BeforeEach(func() {
+			deployVars = map[string]interface{}{
+				"forward_true_client_ip_header": "always_forward",
+			}
+		})
+
+		It("Correctly handles True Client IP when always_forward option is selected", func() {
+			ipAddresses := getAllIpAddresses()
+
+			By("Adding a header with the provided name and correct value for the true client ip, if it doesn't exist in the request")
+			performRequest(client, false, false)
+			expectTrueClientHeader(recordedHeaders, ipAddresses) // Header is added
+
+			By("NOT overwriting the True-Client-Ip header if it is already set AND the request is not a route-service")
+			performRequest(client, false, true)
+			expectTrueClientHeader(recordedHeaders, []string{MockClientIp}) // Header is forwarded
+
+			By("NOT overwriting the True-Client-Ip header if it is already set AND the request is a route-service")
+			performRequest(client, true, true)
+			expectTrueClientHeader(recordedHeaders, []string{MockClientIp}) // Header is forwarded
+		})
 	})
 
-	It("Adds a header with the provided name and correct value for the true client ip, if it doesn't exist", func() {
-		ipAddresses := getAllIpAddresses()
-		request, err = http.NewRequest("GET", "https://haproxy.internal:443", nil)
-		Expect(err).NotTo(HaveOccurred())
+	Context("forward_true_client_ip_header: always_set", func() {
+		BeforeEach(func() {
+			deployVars = map[string]interface{}{
+				"forward_true_client_ip_header": "always_set",
+			}
+		})
 
-		By("Correctly sets the True-Client-Ip Header")
-		resp, err := client.Do(request)
-		expect200(resp, err)
-		headerKey := "X-Cf-True-Client-Ip"
-		Expect(recordedHeaders).To(HaveKey(headerKey))
-		Expect(recordedHeaders[headerKey]).To(HaveLen(1))
-		Expect(ipAddresses).To(ContainElement(recordedHeaders[headerKey][0]))
-	})
+		It("Correctly handles True Client IP when always_set option is selected", func() {
+			ipAddresses := getAllIpAddresses()
 
-	It("Overwrites the True-Client-Ip header if it is already set AND the request is not a route-service", func() {
-		ipAddresses := getAllIpAddresses()
-		request, err = http.NewRequest("GET", "https://haproxy.internal:443", nil)
-		request.Header.Set("X-Cf-True-Client-Ip", "8.8.8.8")
-		Expect(err).NotTo(HaveOccurred())
+			By("Adding a header with the provided name and correct value for the true client ip, if it doesn't exist")
+			performRequest(client, false, false)
+			expectTrueClientHeader(recordedHeaders, ipAddresses) // Header is overwritten
 
-		By("Correctly preserves the pre-existing True-Client-Ip Header")
-		resp, err := client.Do(request)
-		expect200(resp, err)
-		headerKey := "X-Cf-True-Client-Ip"
-		Expect(recordedHeaders).To(HaveKey(headerKey))
-		Expect(recordedHeaders[headerKey]).To(HaveLen(1))
-		Expect(ipAddresses).To(ContainElement(recordedHeaders[headerKey][0]))
-	})
+			By("Overwriting the True-Client-Ip header if it is already set AND the request is NOT a route-service")
+			performRequest(client, false, true)
+			expectTrueClientHeader(recordedHeaders, ipAddresses) // Header is overwritten
 
-	It("Overwrites the True-Client-Ip header if it is already set AND the request is a route-service", func() {
-		ipAddresses := getAllIpAddresses()
-		request, err = http.NewRequest("GET", "https://haproxy.internal:443", nil)
-		// Mock a route-service request via the X-Cf-Proxy-Signature header
-		request.Header.Set("X-Cf-Proxy-Signature", "abc123")
-		request.Header.Set("X-Cf-True-Client-Ip", "8.8.8.8")
-		Expect(err).NotTo(HaveOccurred())
-
-		By("Correctly preserves the pre-existing True-Client-Ip Header")
-		resp, err := client.Do(request)
-		expect200(resp, err)
-		headerKey := "X-Cf-True-Client-Ip"
-		Expect(recordedHeaders).To(HaveKey(headerKey))
-		Expect(recordedHeaders[headerKey]).To(HaveLen(1))
-		Expect(ipAddresses).To(ContainElement(recordedHeaders[headerKey][0]))
+			By("Overwriting the True-Client-Ip header if it is already set AND the request is a route-service")
+			performRequest(client, true, true)
+			expectTrueClientHeader(recordedHeaders, ipAddresses) // Header is overwritten
+		})
 	})
 })
+
+func expectTrueClientHeader(recordedHeaders http.Header, expected []string) {
+	if len(expected) > 0 {
+		Expect(recordedHeaders).To(HaveKey(TrueClientIpHeader))
+		Expect(recordedHeaders[TrueClientIpHeader]).To(HaveLen(1))
+		Expect(expected).To(ContainElement(recordedHeaders[TrueClientIpHeader][0]))
+	} else {
+		Expect(recordedHeaders).NotTo(HaveKey(TrueClientIpHeader))
+	}
+}
+
+func performRequest(client *http.Client, isRouteService bool, hasTrueClientIp bool) {
+	request, err := http.NewRequest("GET", "https://haproxy.internal:443", nil)
+	Expect(err).NotTo(HaveOccurred())
+	if isRouteService {
+		request.Header.Set("X-Cf-Proxy-Signature", "abc123")
+	}
+	if hasTrueClientIp {
+		request.Header.Set(TrueClientIpHeader, MockClientIp)
+	}
+
+	resp, err := client.Do(request)
+	expect200(resp, err)
+}
 
 func getAllIpAddresses() (ips []string) {
 	ifaces, err := net.Interfaces()
