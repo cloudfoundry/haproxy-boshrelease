04929972c82fa5aaa64a3c56c5ee8f8a364ab059
Mike Yeromko <mykhailo.yeromko@sap.com>
2026-03-02 16:12:20 +0100

    POC: Noble and cgroupsv2, rollback ops file

--- ci/scripts/start-bosh.sh
+++ ci/scripts/start-bosh.sh
@@ -247,6 +247,7 @@ EOF
         -o docker/cpi.yml \
         -o jumpbox-user.yml \
         -o /usr/local/local-releases.yml \
+        -o "$PWD/noble-updates.yml" \
         -v director_name=docker \
         -v internal_cidr=${docker_network_cidr} \
         -v internal_gw=10.245.0.1 \
