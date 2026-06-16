01b70777c8557261c51e2169eb0ecf711f0df286
Clemens Hoffmann <clemens.hoffmann@sap.com>
2026-06-16 16:18:34 +0200

    Use cmake binary release instead of bootstrapping from source

--- src/haproxy-versions.sh
+++ src/haproxy-versions.sh
@@ -9,5 +9,5 @@ SOCAT_VERSION=1.8.1.1  # http://www.dest-unreach.org/socat/download/socat-1.8.1.
 HATOP_VERSION=0.8.2  # https://github.com/jhunt/hatop/releases/download/v0.8.2/hatop
 AWS_LC_VERSION=1.72.0  # https://github.com/aws/aws-lc/archive/refs/tags/v1.72.0.tar.gz
 AWS_LC_FIPS_VERSION=3.3.0  # https://github.com/aws/aws-lc/archive/refs/tags/AWS-LC-FIPS-3.3.0.tar.gz
-CMAKE_VERSION=3.31.6  # https://github.com/Kitware/CMake/releases/download/v3.31.6/cmake-3.31.6.tar.gz
+CMAKE_VERSION=3.31.6  # https://github.com/Kitware/CMake/releases/download/v3.31.6/cmake-3.31.6-linux-x86_64.tar.gz
 GOLANG_VERSION=1.26.2  # https://go.dev/dl/go1.26.2.linux-amd64.tar.gz
